5eb9107fea8206a4429a08988c7a874b75f64941
Karan Kapoor <kk8658141@gmail.com>
2022-08-26 12:15:32 +0530

    navbar responsive

--- src/nav-bar/Navbar.js
+++ src/nav-bar/Navbar.js
@@ -1,20 +1,38 @@
 import React from "react";
 import "./Navbar.css";
 import { FaHospitalUser } from 'react-icons/fa';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+
+    const [menuclick, setMenuClick] = useState(false);
+
+    const ChangeValue = () =>
+    {
+        if(!menuclick){
+            setMenuClick(true);
+        }else{
+            setMenuClick(false);
+        }
+    }
+
     return (
         <div>
-        <div className="nav-links">
+        <div className = "nav-links" >
              <h1><FaHospitalUser/> Med-hElp</h1>
-             <ul>
-                <li><a href="/">HOME</a></li>
-                <li><a href="/About">ABOUT</a></li> 
-                <li><a href="/Blog">BLOG</a></li>
-                <li><a href="/Login">LOGIN</a></li>
-                <li><a href="/Doctors">DOCTORS</a></li>
-                <li><a href="/Contact">SUPPORT</a></li>  
+             <ul className={menuclick ? "Menu-nav-links" : "nav-links-ul" }>
+                <li><Link to="/">HOME</Link></li>
+                <li><Link to="/About">ABOUT</Link></li> 
+                <li><Link to="/Blog">BLOG</Link></li>
+                <li><Link to="/Login">LOGIN</Link></li>
+                <li><Link to="/Doctors">DOCTORS</Link></li>
+                <li><Link to="/Contact">SUPPORT</Link></li>  
             </ul>
+            <div className="menu">
+                <p onClick={ChangeValue}><AiOutlineMenu/></p>
+            </div>
         </div>
         </div>
     )
